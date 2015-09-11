6b8a2db8932428ea27e7deb7bc1c321bb3464504
Diwas Adhikary <diadical@diadic.net>
2015-09-11 16:10:08 +1000

    Added more npm dependencies and bower dependencies. Created gulpfile with some basic gulp configuration. Made the server not do anything at this stage.

---
+++ spa-bath/gulpfile.js
@@ -0,0 +1,26 @@
+﻿// gulp and plugins
+var gulp = require('gulp'),
+    gls = require('gulp-live-server');
+
+gulp.task('default', function () {
+    console.log('hello!');
+});
+
+gulp.task('serve', function () {
+    //1. serve with default settings
+    var server = gls.static(); //equals to gls.static('public', 3000);
+    server.start();
+    
+    //2. serve at custom port
+    //var server = gls.static(['dist'], 19000);
+    //server.start();
+    
+    //3. serve multi folders
+    //var server = gls.static(['dist', '.tmp']);
+    //server.start();
+    
+    //use gulp.watch to trigger server actions(notify, start or stop)
+    gulp.watch(['static/**/*.css', 'static/**/*.html'], function (file) {
+        server.notify.apply(server, [file]);
+    });
+});
